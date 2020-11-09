5c67bafcd554024fc025f210882f8d4a53dc9e7c
Aleksey <Aleksey.bortnik91@gmail.com>
2020-11-09 23:06:54 +0300

    Homework-7 is done.

---
+++ assets/js/script.js
@@ -0,0 +1,78 @@
+import modalControl from "./modal.js";
+const modal = modalControl();
+
+let $open = document.querySelector(".add-user");
+let $close = document.querySelectorAll(".close");
+
+$open.addEventListener("click", function () {
+  modal.toggle("open");
+});
+
+for (let $btn of $close) {
+  $btn.addEventListener("click", function () {
+    modal.toggle("close");
+  });
+}
+
+let $table = document.querySelector(".table");
+
+function addUserToTable(user) {
+  let dataRow = `<tr>
+  	<td>${user.firstName}</td>
+  	<td>${user.lastName}</td>
+	  <td>${user.age}</td>
+  </tr>`;
+  $table.innerHTML += dataRow;
+}
+
+let users = [
+  {
+    firstName: "Ashton",
+    lastName: "Kutcher",
+    age: 40,
+  },
+  {
+    firstName: "Bradley",
+    lastName: "Pitt",
+    age: 54,
+  },
+  {
+    firstName: "Hannah",
+    lastName: "Dakota",
+    age: 24,
+  },
+  {
+    firstName: "Roman",
+    lastName: "Aheychik",
+    age: 20,
+  },
+  {
+    firstName: "Valentino",
+    lastName: "Rossi",
+    age: 41,
+  },
+  {
+    firstName: "Macaulay",
+    lastName: "Culkin",
+    age: 40,
+  },
+];
+
+for (let user of users) {
+  addUserToTable(user);
+}
+
+const firstNameInput = document.getElementById("first-name-input");
+const lastNameInput = document.getElementById("last-name-input");
+const ageInput = document.getElementById("age-input");
+
+const $addUserToTableBtn = document.getElementById("add-user-to-table");
+$addUserToTableBtn.addEventListener("click", function () {
+  let newUser = {
+    firstName: firstNameInput.value,
+    lastName: lastNameInput.value,
+    age: ageInput.value,
+  };
+  addUserToTable(newUser);
+  modal.toggle("close");
+});
